1e3d68193c54dcfe56ac773811041367a832f8c4
Mazharul Islam <mazharul3islam0@gmail.com>
2024-06-24 21:00:25 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import { useState } from 'react'
+import './App.css'
+import Blogs from './component/Blogs/Blogs'
+import Bookmarks from './component/Bookmarks/Bookmarks'
+import Header from './component/Header/Header'
+
+
+
+function App() {
+
+  const [bookmarks, setBookmarks] = useState([]);
+  const [readingTime, setReadingTime] = useState(0);
+
+  const handleAddToBookmark = blog => {
+    const newBookmarks = [...bookmarks, blog];
+    setBookmarks(newBookmarks);
+  }
+
+  const handleMarkAsRead = time => {
+    const newReadingTime = readingTime + time;
+    setReadingTime(newReadingTime);
+  }
+
+
+  return (
+    <>
+      <Header></Header>
+
+      <div className='md:flex max-w-5xl mx-auto'>
+        <Blogs handleAddToBookmark={handleAddToBookmark} handleMarkAsRead={handleMarkAsRead}></Blogs>
+        <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Blog/Blog.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import { PiBookmarkSimpleFill } from "react-icons/pi";
+
+const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
+    const { title, cover, author, author_img, posted_date, reading_time, hashtags } = blog;
+    return (
+        <div className='mb-8 space-y-4'>
+            <img className='w-full mb-8 mt-3 h-96 rounded-md' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between mb-4'>
+                <div className='flex'>
+                    <img className='h-14 w-14 rounded-full' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button
+                        onClick={() => handleAddToBookmark(blog)}
+                        className='ml-2 text-2xl text-red-600'
+                    >
+                        <PiBookmarkSimpleFill />
+                    </button>
+                </div>
+            </div>
+            <h2 className="4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">{hash}</a></span>)
+                }
+            </p>
+            <button
+                onClick={() => handleMarkAsRead(reading_time)}
+                className='text-purple-800 font-bold underline'
+            >
+                Mark As Read
+            </button>
+        </div>
+    );
+};
+
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func,
+    handleMarkAsRead: PropTypes.func
+}
+
+export default Blog;
